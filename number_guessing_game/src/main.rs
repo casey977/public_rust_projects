df8a766716f0adee6db96cfde22f3f6308e78d11
Casey Meyvn <shatternight@protonmail.com>
2023-10-12 22:10:55 +0200

    Add var for storing user inputs

--- number_guessing_game/src/main.rs
+++ number_guessing_game/src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 
 fn main() {
 	let cmd_args:Vec<String> = env::args().collect();
+	let mut input = String::new();
 
 	println!("Guess a number between 0 and 100 (both inclusive): ");
 }
