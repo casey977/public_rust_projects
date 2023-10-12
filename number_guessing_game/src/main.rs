f86a6a31200a4cb924f35b7417e6b60b35dad224
Casey Meyvn <shatternight@protonmail.com>
2023-10-12 23:02:45 +0200

    Read cmd input

--- number_guessing_game/src/main.rs
+++ number_guessing_game/src/main.rs
@@ -4,6 +4,9 @@ use std::io;
 fn main() {
 	let cmd_args:Vec<String> = env::args().collect();
 	let mut input = String::new();
+	
+	io::stdin().read_line(&mut input)
+		.expect("ERROR: Failed to read line!");
 
 	println!("Guess a number between 0 and 100 (both inclusive): ");
 }
